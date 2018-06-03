64e000f64f17613256b40f3f914c94e2726baa86
Artem Hlumov <artyom.altair@gmail.com>
2018-06-03 22:35:59 +0200

    Light parameters

--- glwindow.h
+++ glwindow.h
@@ -208,7 +208,7 @@ public:
             100.0f             // Дальняя плоскость отсечения.
         );
 
-        glm::vec3 lightPosition3{-10.0, 10.0, -10.0};
+        glm::vec3 lightPosition3{0.0, 1.0, -10.0};
 
         //glm::mat4 lightModelMatrix = glm::rotate(glm::mat4(1.0f), float(-glfwGetTime() * 3), glm::vec3(0, 1, 0));
         //lightPosition4 = lightModelMatrix * lightPosition4;
@@ -450,12 +450,12 @@ private:
            vec3 ambientColor = textureColor * 0.2;
 
            // Diffuse
-           float diffuseFactor = max(dot(normVec.xyz, lightDirection), 0) * .5;
+           float diffuseFactor = max(dot(normVec.xyz, lightDirection), 0);
            vec3 diffuseColor = textureColor * diffuseFactor;
 
            // Specular
            vec3 R = 2*dot(normVec.xyz, lightDirection) * normVec.xyz - lightDirection;
-           float specularFactor = max((dot(R, cameraDirection) * .5), 0);
+           float specularFactor = max((dot(R, cameraDirection) * .5), 0) * .5;
            vec3 specularColor = textureColor * specularFactor;
 
 //           if (dot(norm.xyz, lightDirection) > 0) {
